2a176a7878c8f820b298d040544c95213215957f
JorgeAl <73070535+jorgeall@users.noreply.github.com>
2022-06-30 18:42:36 -0300

    Update Clase13_ejercicio3_hilo.py

--- Clase13_ejercicio3_hilo.py
+++ Clase13_ejercicio3_hilo.py
@@ -7,8 +7,8 @@ import time
 exitFlag = 0
 
 class myThread (threading.Thread):
-    def init(self, threadID, name, counter):
-        threading.Thread.init(self)
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.counter = counter
