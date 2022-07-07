f31c705ac1cee8c6d98e40691a3f9829cb325724
JorgeAl <73070535+jorgeall@users.noreply.github.com>
2022-07-07 19:17:59 -0300

    Add files via upload

---
+++ lista2.py
@@ -0,0 +1,10 @@
+import bisect
+scores = [(100, 'perl'), (200, 'tcl'), (400, 'lua'), (500, 'python')]
+bisect.insort(scores, (300, 'ruby'))
+print(scores)
+from heapq import heapify, heappop, heappush
+data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+heapify(data)                      # rearrange the list into heap order
+heappush(data, -5)                 # add a new entry
+lista=[heappop(data) for i in range(6)]  # fetch the three smallest entries
+print(lista)
