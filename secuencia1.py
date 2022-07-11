7ceacab5e104795deb6c03774d5fbb141dcec7a4
JorgeAl <73070535+jorgeall@users.noreply.github.com>
2022-07-11 19:38:49 -0300

    Add files via upload

---
+++ secuencia1.py
@@ -0,0 +1,10 @@
+print("gg" in "eggs")
+lists = [[]] * 3
+print(lists)
+lists[0].append(3)
+print(lists)
+lists = [[] for i in range(3)]
+lists[0].append(3)
+lists[1].append(5)
+lists[2].append(7)
+print(lists)
